5b05377bda59299a4379155fbd99bc535013a5fd
Sungdoo Yoo <dev.esung@gmail.com>
2020-10-24 12:16:26 +0900

    Initial Commit

---
+++ FindBottleNeck/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FindBottleNeck
+//
+//  Created by 류성두 on 2020/10/24.
+//  Copyright © 2020 rainist. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
